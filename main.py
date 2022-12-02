7762cc15c1171a65c94ebe065b019fec940c1701
Opstober <opstober@duck.com>
2022-12-02 19:37:22 +0700

    Huge Change

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-import os, binascii, hashlib
+import os, hashlib 
 
 os.system('cls' if os.name == 'nt' else 'clear')
-value = input("Enter the text you want to hash:\n")
 
+algorithm = input('Enter hash algorithm ("?" for available algorithm):\n')
+algorithm = algorithm.lower()
 os.system('cls' if os.name == 'nt' else 'clear')
-algorithm = input('Enter hash algorithm (currently only support SHA256 and SHA512:\n')
 
-os.system('cls' if os.name == 'nt' else 'clear')
-if algorithm == 'sha256' or algorithm == 'SHA256':
-    hash = hashlib.sha256(value.encode('utf-8'))
-elif algorithm == 'sha512' or algorithm == 'SHA512':
-    hash = hashlib.sha512(value.encode('utf-8'))
-else:
-    print(f'\nWTF is {algorithm}???\n')
+supported_algorithm = hashlib.algorithms_available
+
+if algorithm == '?':
+    print(hashlib.algorithms_available)
+    exit()
+elif algorithm not in supported_algorithm:
+    print(f'\nWTF is {algorithm}!?\n')
     exit()
 
+value = input('Enter the text you want to hash:\n')
+os.system('cls' if os.name == 'nt' else 'clear')
+
+hash = hashlib.new(f'{algorithm}')
+hash.update(value.encode('utf-8'))
 hashed = hash.hexdigest()
 
-os.system('cls' if os.name == 'nt' else 'clear')
 print(f'\nYou choosed: {algorithm}\nHash: {hashed}\n')
